0233de9b8f5a33537cc8255588b77e64d211a46c
sandeep-kalasgonda <sandeeprajendra00@gmail.com>
2022-12-20 22:00:35 +0530

    container properties

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return (MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue[200],
-        appBar: AppBar(
-          title: const Text("sandeep"),
-          backgroundColor: Colors.red[300],
-        ),
-        body: const Center(
-          child: Image(image: AssetImage('images/diamond.jpeg')),
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Container(
+            width: 100.0,
+            height: 100.0,
+            margin:
+                const EdgeInsets.symmetric(horizontal: 100.0, vertical: 100.0),
+            padding: const EdgeInsets.all(20.0),
+            color: Colors.amber,
+            child: const Text('Farmer'),
+          ),
         ),
       ),
-    ),
-  );
+    ));
+  }
 }
